3cc348bbc7a8ab15f913339a4370d062d07d21e1
Sébastien NEDJAR <sebastien@nedjar.com>
2017-05-23 05:09:30 +0200

    Correction test exo4

--- src/test/java/fr/univ_amu/iut/exercice4/TestApplicationLifeCycle.java
+++ src/test/java/fr/univ_amu/iut/exercice4/TestApplicationLifeCycle.java
@@ -1,34 +1,44 @@
 package fr.univ_amu.iut.exercice4;
 
+import fr.univ_amu.iut.exercice5.WhoIsWho;
+import javafx.stage.Stage;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
 
 import java.io.PrintStream;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-public class TestApplicationLifeCycle {
+public class TestApplicationLifeCycle extends ApplicationTest {
 
-    private ApplicationLifeCycle applicationLifeCycle = new ApplicationLifeCycle();
+    private PrintStream out;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.setupApplication(WhoIsWho.class);
+    }
 
     @Ignore
     @Test
     public void constructor() throws Exception {
         PrintStream out = mock(PrintStream.class);
         System.setOut(out);
-
         new ApplicationLifeCycle();
         verify(out).println("constructeur ApplicationLifeCycle()");
     }
+
 
     @Ignore
     @Test
     public void init() throws Exception {
         PrintStream out = mock(PrintStream.class);
         System.setOut(out);
-
-        applicationLifeCycle.init();
+        new ApplicationLifeCycle().init();
         verify(out).println("init()");
     }
 
@@ -37,18 +47,24 @@ public class TestApplicationLifeCycle {
     public void stop() throws Exception {
         PrintStream out = mock(PrintStream.class);
         System.setOut(out);
-
-        applicationLifeCycle.stop();
+        new ApplicationLifeCycle().stop();
         verify(out).println("stop()");
     }
 
     @Ignore
     @Test
-    public void start() throws Exception {
-        PrintStream out = mock(PrintStream.class);
-        System.setOut(out);
-
-        applicationLifeCycle.start(null);
-        verify(out).println("start()");
+    public void test_life_cycle_output() {
+        verify(out).println("constructeur ApplicationLifeCycle()");
+        verify(out).println("start() : avant show stage");
+        verify(out).println("start() : après show stage");
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        out = mock(PrintStream.class);
+        System.setOut(out);
+        new ApplicationLifeCycle().start(stage);
+    }
+
+
 }
