ec214e0787823dc5213428c31598b5c3223d8281
FERRER Vincent <f18000711@a-maq-iutinfo.salsa.univ-amu.fr>
2019-05-10 08:46:41 +0200

    Validation exercice 10

--- src/main/java/fr/univ_amu/iut/exercice10/HelloBeautifulUsefulButton.java
+++ src/main/java/fr/univ_amu/iut/exercice10/HelloBeautifulUsefulButton.java
@@ -1,13 +1,45 @@
 package fr.univ_amu.iut.exercice10;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class HelloBeautifulUsefulButton extends Application {
     private int nbAction = 0;
 
+    EventHandler<ActionEvent> ecouteur = new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            nbAction += 1;
+            System.out.println("Bouton actionné " + nbAction + " fois");
+        }
+    };
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setTitle("Hello !");
+        primaryStage.setWidth(250);
+        primaryStage.setHeight(250);
+        Button but = new Button("Hello !");
+        but.setId("buttonHello");
+        Scene scene = new Scene(new BorderPane(but));
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("DarkTheme.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        ImageView ImgURL = new ImageView("https://raw.githubusercontent.com/IUTInfoAix-M2105/Syllabus/master/assets/logo.png");
+        but.setGraphic(ImgURL);
+        but.setOnAction(ecouteur);
     }
+
+
+
 }
+
+
+
+
